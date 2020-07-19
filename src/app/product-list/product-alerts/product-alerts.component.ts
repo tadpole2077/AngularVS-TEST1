d3fef7d0ade1ef0e089186e465cb88ecabf7407a
tadpole2077 <pobrien@tadpolesoftware.com>
2020-07-19 18:34:44 +0100

    4 files modified

--- src/app/product-list/product-alerts/product-alerts.component.ts
+++ src/app/product-list/product-alerts/product-alerts.component.ts
@@ -9,6 +9,7 @@ import { Output, EventEmitter } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
